332714b6f5dc719e062f0282713541d2218eb8cc
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2019-09-17 18:06:58 -0300

    Cambia configuracion inventory

--- infraestructura/provision.sh
+++ infraestructura/provision.sh
@@ -3,4 +3,4 @@ set -e -u
 
 INVENTORY_FILE=$1
 
-ansible-playbook ./infraestructura/playbook.yml -i $INVENTORY_FILE --extra-vars "usermanager=usuario passmanager=clave"
+ansible-playbook ./infraestructura/playbook.yml -i $INVENTORY_FILE -vvvv --extra-vars "usermanager=usuario passmanager=clave"
