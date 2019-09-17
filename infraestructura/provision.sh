67013ad3e4102088a7f6cef67bf6e3d0d19c1fc0
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2019-09-17 17:03:59 -0300

    Cambia path de playbook en provision.sh

--- infraestructura/provision.sh
+++ infraestructura/provision.sh
@@ -3,4 +3,4 @@ set -e -u
 
 INVENTORY_FILE=$1
 
-ansible-playbook playbook.yml -i $INVENTORY_FILE --extra-vars "usermanager=usuario passmanager=clave"
+ansible-playbook ./infraestructura/playbook.yml -i $INVENTORY_FILE --extra-vars "usermanager=usuario passmanager=clave"
